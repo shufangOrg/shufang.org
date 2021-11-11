68f0da46a0f10d95446775ec6ee2f3031f2ac65d
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2021-11-11 18:30:19 +0800

    Update config.el

--- scripts/config.el
+++ scripts/config.el
@@ -3,4 +3,4 @@
    (with-current-buffer
        (find-file-noselect x)
      (org-hugo-export-wim-to-md t)))
- (directory-files "./org" nil "^[0-9]+$" t))
+ (directory-files "../org" nil "^[0-9]+$" t))
