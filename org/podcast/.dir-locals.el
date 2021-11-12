a1c94b1a01e4af3053b5b7891c1800573a4e58d2
Jason Tian <kuohuitien@gmail.com>
2021-11-12 13:38:00 +0800

    moved the ox-hugo front-matter to .dir-locals.el

--- org/podcast/.dir-locals.el
+++ org/podcast/.dir-locals.el
@@ -3,5 +3,7 @@
 
 (
  (org-mode . ((eval . (org-hugo-auto-export-mode))))
- (org-mode . ((org-hugo-base-dir . "../..")))
+ (org-mode . ((org-hugo-base-dir . "../..")
+	      (org-hugo-section . "podcast")
+	      ))
  )
