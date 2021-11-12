b99ce3c71590b7036ef6b167c948ba4c7e6d05d5
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2021-11-12 12:00:39 +0800

    Update config.el

--- scripts/config.el
+++ scripts/config.el
@@ -1,8 +1,11 @@
 (defun build/export-all ()
- (mapc
- (lambda (x)
-   (with-current-buffer
-       (find-file-noselect x)
-     (org-hugo-export-wim-to-md t)))
- (directory-files "./org" nil "^[0-9]+$" t))
- )
+  "Export all org-files (including nested) under notes-org-files."
+
+(setq notes-org-files "./org/")
+
+  (dolist (org-file (directory-files-recursively notes-org-files "\.org$"))
+    (with-current-buffer (find-file org-file)
+      (message (format "[build] Exporting %s" org-file))
+      (org-hugo-export-wim-to-md :all-subtrees nil nil nil)))
+
+  (message "Done!"))
