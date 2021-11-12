bb0d589680c939597951fceb89ab0e98934a3e83
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2021-11-12 11:28:49 +0800

    Update config.el

--- scripts/config.el
+++ scripts/config.el
@@ -1,6 +1,8 @@
-(mapc
+(defun build/export-all ()
+ (mapc
  (lambda (x)
    (with-current-buffer
        (find-file-noselect x)
      (org-hugo-export-wim-to-md t)))
  (directory-files "./org" nil "^[0-9]+$" t))
+ )
