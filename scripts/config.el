e144c5154cec39c7e12b9793d59829e5ca3b7b5f
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2021-11-12 12:33:04 +0800

    Update config.el

--- scripts/config.el
+++ scripts/config.el
@@ -3,7 +3,8 @@
 
 (setq notes-org-files "./org/")
 
-  (dolist (org-file (directory-files-recursively notes-org-files "\.org$"))
+ ;; (dolist (org-file (directory-files-recursively notes-org-files "\.org$"))
+  (dolist (org-file (directory-files notes-org-files nil "\.org$" t))
     (with-current-buffer (find-file org-file)
       (message (format "[build] Exporting %s" org-file))
       (org-hugo-export-wim-to-md)))
