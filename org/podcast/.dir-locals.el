e1fc583c972fd2f9be7820066be98e7c1dfd291d
Jason Tian <kuohuitien@gmail.com>
2022-03-29 16:25:26 +0800

    removed org files

--- org/podcast/.dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-(
- (org-mode . ((eval . (org-hugo-auto-export-mode))))
- (org-mode . ((org-hugo-base-dir . "../..")
-	      (org-hugo-section . "podcast")
-	      ))
- )
