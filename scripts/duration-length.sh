51041f48bbe6b5b63247b1a3c06bd6bf37aa4f3c
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2022-04-02 22:35:51 +0800

    Update duration-length.sh

--- scripts/duration-length.sh
+++ scripts/duration-length.sh
@@ -2,7 +2,7 @@ file="./scripts/duration-length.txt"
 if [ -f "$file" ]
 then
     for i in $(cat $file); do
-        mp3=$(cat "$i" | grep "^mp3 =" | awk -F= {print $2})
+        mp3=$(cat "$i" | grep "^mp3 =" | awk -F" '{print $2}')
         duration=$(ffprobe -i "$mp3" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)
         length=$(ffprobe -i "$mp3" -show_entries format=size -v quiet -of csv="p=0")
         sed -i 's/^duration = .*/duration = \"($duration)\"/g' "$i"
