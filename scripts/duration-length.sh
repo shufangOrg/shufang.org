258d4cb72ac1ee4d292ce2628b7687e9cd3bf2be
Jason Tian <kuohuitien@gmail.com>
2023-03-08 09:30:37 +0800

    fix the script

--- scripts/duration-length.sh
+++ scripts/duration-length.sh
@@ -2,28 +2,26 @@ file="./scripts/duration-length.txt"
 # duration-length.txt example
 # https://github.com/shufangOrg/shufang.org/commit/bb08abed58cb35e530988eb44c2683c6327578e1
 if [ -f "$file" ]
-   FIRSTLINE=$(head -n 1 "$file")
 then
-    if [ ${FIRSTLINE:0:3} == "---" ]
-    then
-	for i in $(cat $file); do
+    for i in $(cat $file); do
+	FIRSTLINE=$(head -n 1 "$i")
+	if [ ${FIRSTLINE:0:3} == "---" ]
+	then
 	    mp3="https://"$(cat "$i" | grep "^mp3:" | awk -F' ' '{print $2}')
 	    duration=$(ffprobe -i "$mp3" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)
 	    length=$(ffprobe -i "$mp3" -show_entries format=size -v quiet -of csv="p=0")
 	    sed -i "s/^duration: .*/duration: \"$duration\"/g" "$i"
 	    sed -i "s/^length: .*/length: $length/g" "$i"
 	    cat "$i"
-	done
-    fi
-    if [ ${FIRSTLINE:0:3} == "+++" ]
-    then
-	for i in $(cat $file); do
+	fi
+	if [ ${FIRSTLINE:0:3} == "+++" ]
+	then
 	    mp3="https://"$(cat "$i" | grep "^mp3 =" | awk -F' ' '{print $2}')
 	    duration=$(ffprobe -i "$mp3" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)
 	    length=$(ffprobe -i "$mp3" -show_entries format=size -v quiet -of csv="p=0")
 	    sed -i "s/^duration = .*/duration = \"$duration\"/g" "$i"
 	    sed -i "s/^length = .*/length = $length/g" "$i"
 	    cat "$i"
-	done
-    fi
+	fi
+    done
 fi
