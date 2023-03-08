12cf7820156c8225075145463fe0ebd75def1910
Jason Tian <kuohuitien@gmail.com>
2023-03-08 09:37:14 +0800

    fix the script to deal with toml format

--- scripts/duration-length.sh
+++ scripts/duration-length.sh
@@ -16,7 +16,7 @@ then
 	fi
 	if [ ${FIRSTLINE:0:3} == "+++" ]
 	then
-	    mp3="https://"$(cat "$i" | grep "^mp3 =" | awk -F' ' '{print $2}')
+	    mp3="https://"$(cat "$i" | grep "^mp3 =" | awk -F'"' '{print $2}')
 	    duration=$(ffprobe -i "$mp3" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)
 	    length=$(ffprobe -i "$mp3" -show_entries format=size -v quiet -of csv="p=0")
 	    sed -i "s/^duration = .*/duration = \"$duration\"/g" "$i"
