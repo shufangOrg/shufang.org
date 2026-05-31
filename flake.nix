18acdaffdd06103a775ac6053c6689b7c5133d9f
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2026-05-31 23:08:42 +0800

    Add flake.nix for reproducible CI builds

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "shufang.org Hugo site build environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.hugo
+            pkgs.ffmpeg
+            pkgs.git
+            pkgs.gnused
+            pkgs.gawk
+            pkgs.openssh
+          ];
+        };
+      });
+}
