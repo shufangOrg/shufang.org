cee5848801ca7fbcf17009a2d9f0fc5dcbd942f6
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2026-05-31 23:11:26 +0800

    Pin Hugo to 0.87.0 extended in flake.nix

--- flake.nix
+++ flake.nix
@@ -10,11 +10,24 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        hugo = pkgs.stdenv.mkDerivation rec {
+          pname = "hugo";
+          version = "0.87.0";
+          src = pkgs.fetchurl {
+            url = "https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_extended_${version}_Linux-64bit.tar.gz";
+            sha256 = "sha256-L4eaGCkm/Qd4Kp/bfMRVNdCYjPxKiEOkzsOmKSgD/s=";
+          };
+          sourceRoot = ".";
+          installPhase = ''
+            mkdir -p $out/bin
+            cp hugo $out/bin/
+          '';
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.hugo
+            hugo
             pkgs.ffmpeg
             pkgs.git
             pkgs.gnused
